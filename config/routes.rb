ab2efc02ffef2fda12aec6dffe357400c55e9d60
S-tanishi <tanichi.u.n.20@gmail.com>
2021-11-02 21:49:26 +0900

    add: sample routing 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,46 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+
+  # resources :controller名\
+  # resources :controller名, only: [:index, :show]
+
+#   resources :tweets
+#   $ rake routes
+
+#     Prefix   Verb   URI Pattern                 Controller#Action
+#     tweets   GET    /tweets(.:format)           tweets#index
+#              POST   /tweets(.:format)           tweets#create   *Laravel でいうstoreにあたる 
+#  new_tweet   GET    /tweets/new(.:format)       tweets#new　　　 *Laravel でいうcreate
+# edit_tweet   GET    /tweets/:id/edit(.:format)  tweets#edit
+#      tweet   GET    /tweets/:id(.:format)       tweets#show
+#              PATCH  /tweets/:id(.:format)       tweets#update
+#              PUT    /tweets/:id(.:format)       tweets#update
+#              DELETE /tweets/:id(.:format)       tweets#destroy
+
+#    resource :tweet
+#      Prefix   Verb   URI Pattern            Controller#Action
+#   new_tweet   GET    /tweet/new(.:format)   tweets#new
+#  edit_tweet   GET    /tweet/edit(.:format)  tweets#edit
+#       tweet   GET    /tweet(.:format)       tweets#show
+#               PATCH  /tweet(.:format)       tweets#update
+#               PUT    /tweet(.:format)       tweets#update
+#               DELETE /tweet(.:format)       tweets#destroy
+#               POST   /tweet(.:format)       tweets#create
+
+# 違いは以下２点
+# ・indexアクションが定義されないこと
+# ・URI Patternにidを含まないこと
+
+
+#  ネスト
+# resources :contents do
+#   resources :comments
+# end
+
+# どのcontentに対してのcommentであるかをルーティングで紐づけることができる。
+# $ rake routes
+# content_comments  POST  /contents/:content_id/comments(.:format)    comments#create
+
+
 end
