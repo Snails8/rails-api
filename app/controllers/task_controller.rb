807d0e1e17a86cee595abee6f09f7d810fa1c0fc
S-tanishi <tanichi.u.n.20@gmail.com>
2021-11-28 19:27:51 +0900

    add: タスク作成処理の作成

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -5,6 +5,25 @@ class TaskController < ApplicationController
     @tasks = Task.all
   end
 
+  # タスク追加画面（/tasks/new）にアクセスするときに呼ばれる
+  def new
+    @task = Task.new
+  end
   
+  # 作成(タスク追加するときに呼ばれる)
+  def create
+    @task = Task.create(task_params)
+    #  名前付きルート
+    #  tasks_pathはトップ画面（/）へのリンクを表す
+    redirect_to task_path
+  end
   
+  # Strong Parameters
+  # ユーザから悪意のあるパラメータが渡ってきても安全に処理してくれるもの
+  # Taskモデルのtitle（タスク名）パラメータがViewから渡ってきた場合のみタスク追加を行う
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
+    
 end
